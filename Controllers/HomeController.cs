ef19fb97bf574f3cfb573f417d45d319fad1970d
wwweijian <simweikian729@gmail.com>
2025-12-05 17:34:17 +0800

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,24 @@ namespace Mini_Project.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Add your login logic here
+            if (username == "admin" && password == "password")
+            {
+                // Successful login
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Failed login
+                ViewData["Error"] = "Invalid username or password";
+                return View("Index");
+            }
+        }
+
         public IActionResult Error() => View();
     }
 }
